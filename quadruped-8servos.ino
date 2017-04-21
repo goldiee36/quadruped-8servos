e7d3e566a8adc8dd4b5856e7634ec1a3adaee7f1
goldiee36 <boldizsar.adam@gmail.com>
2017-04-21 17:26:32 +0200

    merge?

--- quadruped-8servos.ino
+++ quadruped-8servos.ino
@@ -119,15 +119,8 @@ void setup()
   
   //delay(2000);
   movementStartTime = millis();
-<<<<<<< HEAD
-  setServoMovement();
-  delay(5000);
-  movementStartTime = millis();
-  setServoMovement();
-=======
   //setServoMovement();
   //delay(1000000);
->>>>>>> 964beb3b87b873ff855cc42e3da1e0a851fccd48
 }
 
 
@@ -138,9 +131,9 @@ void setup()
 unsigned int movementSpeed = 10000; //time for advancing for all 4 leg. bigger values are slower
 
  //move forward values:
-byte legOrder[] = {4, 1, 3, 2};
+/*byte legOrder[] = {4, 1, 3, 2};
 int hipMin[] = {-23, 45, -23, 45}; //for corresponding legs in the legOrder list
-int hipMax[] = {45, -23, 45, -23};
+int hipMax[] = {45, -23, 45, -23};*/
 
  //move backward values:
 /*byte legOrder[] = {2, 3, 1, 4};
@@ -157,7 +150,6 @@ int hipMax[] = {23, 90, 23, 90};*/
 int hipMin[] = {90, 23, 90, 23}; //for corresponding legs in the legOrder list
 int hipMax[] = {23, 90, 23, 90};*/
 
-<<<<<<< HEAD
  //turn left values:
 /*byte legOrder[] = {1, 2, 3, 4};
 int hipMin[] = {68, 0, 68, 0}; //for corresponding legs in the legOrder list
@@ -167,7 +159,7 @@ int hipMax[] = {0, 68, 0, 68};*/
 byte legOrder[] = {4, 3, 2, 1};
 int hipMin[] = {68, 0, 68, 0}; //for corresponding legs in the legOrder list
 int hipMax[] = {0, 68, 0, 68};
-=======
+
 int calcHipValues[4] = {-100,-100,-100,-100};
 
 void loop() {  
@@ -198,8 +190,6 @@ int hipMax[] = {23, 90, 23, 90};
   Serial.print("Calc time: ");Serial.println(endmicro-startmicro);
   delay(200000);
 }
-
->>>>>>> 964beb3b87b873ff855cc42e3da1e0a851fccd48
 
 void setServoMovement() {
   servoValues(0, movementStartTime, millis(), movementSpeed, legOrder, hipMin, hipMax);
